4269c1b94f265e754641c9cb89b91e103158ff2e
ardhendu66 <ardhenduroy2002@gmail.com>
2024-07-24 00:48:13 +0530

    cors error fixed

--- src/routes/adminProfileRoute.js
+++ src/routes/adminProfileRoute.js
@@ -3,7 +3,7 @@ import { updateProfileImage } from "../controllers/user.js";
 import { authorization } from "../utils/authorization.js";
 const router = express.Router();
 
-router.get("/", authorization, checkSession);
+// router.get("/", authorization, checkSession);
 router.put("/profile", authorization, updateProfileImage);
 
 export default router;
